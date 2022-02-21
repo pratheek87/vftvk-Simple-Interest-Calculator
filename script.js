6ea442e82558a02cee18120858d91117f20e4fb1
pratheek87 <pratheek87@gmail.com>
2022-02-22 03:39:39 +0530

    Check-in

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var slider = document.getElementById("rate");
 var op = document.getElementById("op");
 op.innerHTML = slider.value;
 
-slider.oninput() = function () {
+slider.oninput = function () {
     op.innerHTML = this.value;
 }
 
